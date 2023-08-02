ac9f0ad6725aa9d184dc80e5e35c19e216b5d7a1
Robert Cunningham <robertvcunningham@gmail.com>
2023-08-01 23:15:13 -0700

    pull out attention_modules for broader use

---
+++ attention_modules.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange, reduce, repeat
+
+dt = torch.bfloat16
+
+
+class SaveAttention(nn.Module):
+    def __init__(self, base, index, **kwargs):
+        super().__init__()
+        self.base_attn = base
+        self.history = []
+
+    def forward(self, hidden_states, encoder_hidden_states=None, attention_mask=None, **cross_attention_kwargs):
+        out = self.base_attn.forward(hidden_states, encoder_hidden_states, attention_mask, **cross_attention_kwargs)
+        self.history.append(out.detach())
+        return out
+
+    def __getattr__(self, name):
+        try:
+            return super().__getattr__(name)
+        except:
+            return self.base_attn.__getattribute__(name)
+
+
+class CorrespondingAttention(nn.Module):
+    def __init__(self, base, index, **kwargs):
+        super().__init__()
+        self.base_attn = base
+        self.index = index
+        self.attn_transitions = kwargs["attn_transitions"]  # layer batch { }
+        self.active_frames = kwargs["active_frames"]  # batch
+        self.keyframe_attns = kwargs["keyframe_attns"]  # layer batch hw c
+        self.tb_true_to_batch_index = kwargs["tb_true_to_batch_index"]  # batch
+        self.history = None
+
+    def forward(self, hidden_states, encoder_hidden_states=None, attention_mask=None, **cross_attention_kwargs):
+        my_layer_corr = self.attn_transitions[self.index]
+        my_layer_keyframe_attns = self.keyframe_attns[self.index]  # batch hw c
+
+        batch_before_key_frame_indexes = torch.tensor([self.tb_true_to_batch_index[c["before_frame"]] for c in my_layer_corr])
+        batch_after_key_frame_indexes = torch.tensor([self.tb_true_to_batch_index[c["after_frame"]] for c in my_layer_corr])
+
+        before_keyframe_attns = my_layer_keyframe_attns[batch_before_key_frame_indexes]
+        after_keyframe_attns = my_layer_keyframe_attns[batch_after_key_frame_indexes]
+
+        before_shuffled = torch.stack([b[c["before"]] for b, c in zip(before_keyframe_attns, my_layer_corr)])
+        after_shuffled = torch.stack([a[c["after"]] for a, c in zip(after_keyframe_attns, my_layer_corr)])
+
+        w = torch.tensor([(c["center_frame"] - c["before_frame"]) / (c["after_frame"] - c["before_frame"]) for c in my_layer_corr]).cuda().to(dt)
+
+        out = torch.einsum("ijk,i->ijk", [before_shuffled, 1 - w]) + torch.einsum("ijk,i->ijk", [after_shuffled, w])
+
+        self.history = out.detach()
+
+        return out
+
+    def __getattr__(self, name):
+        try:
+            return super().__getattr__(name)
+        except AttributeError:
+            return self.base_attn.__getattribute__(name)
+
+
+class ExtendedAttention(nn.Module):
+    def __init__(self, base, index):
+        super().__init__()
+        self.index = index
+        self.base_attn = base
+        self.history = None
+
+    def forward(self, hidden_states, encoder_hidden_states=None, attention_mask=None, **cross_attention_kwargs):
+        # we batch by polarity (positive / negative prompt), so positive and negative prompts don't cross-attend.
+
+        b = hidden_states.shape[0]
+        hidden_states = rearrange(hidden_states, "b hw c -> 1 (b hw) c")
+        if encoder_hidden_states is not None:
+            encoder_hidden_states = reduce(encoder_hidden_states, "b t c -> 1 t c", "mean")
+        out = self.base_attn.forward(hidden_states, encoder_hidden_states, attention_mask, **cross_attention_kwargs)
+        out = rearrange(out, "1 (b hw) c -> b hw c", b=b)
+
+        self.history = out.detach()  # out.chunk(2)[1].detach()
+        return out
+
+    def __getattr__(self, name):
+        try:
+            # we need this to search _modules, where torch moves otherwise-normal properties.
+            # normally you can just do super().__getattr__(name) but that doesn't work here.
+            # since .base_attn isn't in the __dict__, and will fallthrough to __getattr__.
+            return super().__getattr__(name)
+        except AttributeError:
+            return self.base_attn.__getattribute__(name)
+
+
+class NormalAttention(nn.Module):
+    def __init__(self, base, index, **kwargs):
+        super().__init__()
+        self.base_attn = base
+        self.history = None
+
+    def forward(self, hidden_states, encoder_hidden_states=None, attention_mask=None, **cross_attention_kwargs):
+        out = self.base_attn.forward(hidden_states, encoder_hidden_states, attention_mask, **cross_attention_kwargs)
+        self.history = out.detach()
+        return out
+
+    def __getattr__(self, name):
+        try:
+            return super().__getattr__(name)
+        except AttributeError:
+            return self.base_attn.__getattribute__(name)
---
+++ utils.py
@@ -0,0 +1,40 @@
+import torch
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors
+from PIL import Image
+
+def generate_circular_gradient(size):
+    # Define a blank array with zeros, now with dtype uint8
+    image = np.zeros((size, size, 3), dtype=np.uint8)
+
+    # Define the center of the image
+    center = [size//2, size//2]
+
+    # Create the gradient
+    for x in range(image.shape[0]):
+        for y in range(image.shape[1]):
+            # Hue
+            angle = np.arctan2(center[0]-x, center[1]-y) 
+            h = (angle + np.pi) / (2 * np.pi)  # Adjust range to [0, 1]
+            # Saturation: use distance from center
+            s = np.sqrt((x - center[0])**2 + (y - center[1])**2) / (np.sqrt(2) * size//2)
+            s = np.clip(s, 0, 1)  # Ensure saturation stays within [0, 1]
+            # Value / Brightness
+            v = 1  # Keep constant
+            # Convert HSV to RGB and then scale to [0, 255]
+            rgb = matplotlib.colors.hsv_to_rgb([h, s, v])
+            image[x, y] = np.array(rgb * 255, dtype=np.uint8)
+
+    return Image.fromarray(image)
+
+def display_images(images):
+	plt.figure(figsize=(10,10)) # specifying the overall grid size
+
+	for i in range(len(images)):
+		plt.subplot(5,5,i+1)    # the number of images in the grid is 5*5 (25)
+		plt.axis('off')
+		plt.imshow(images[i])
+
+	plt.show()
